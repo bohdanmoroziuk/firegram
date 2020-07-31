bc8fa9d891535e44b784088693fde6b159b16696
Bogdan <bogdan7morozyuk@gmail.com>
2020-07-31 10:14:02 +0300

    chore(firebase): configure

---
+++ src/firebase/initialize.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+import config from 'firebase/config';
+
+firebase.initializeApp(config);
+
+export const storage = firebase.storage();
+export const firestore = firebase.firestore();
