2c0a37e69c86446e135e8b3ddc9942a819f7acd2
Bogdan <bogdan7morozyuk@gmail.com>
2020-08-02 09:32:09 +0300

    style(progress-bar): remove curly braces

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import 'components/ProgressBar/index.css';
 
-const ProgressBar = ({ progress }) => {
-
-  return (
-    <motion.div 
-      className="progress-bar"
-      initial={{
-        width: 0
-      }}
-      animate={{
-        width: `${progress}%`
-      }}
-    />
-  );
-};
+const ProgressBar = ({ progress }) => (
+  <motion.div 
+    className="progress-bar"
+    initial={{ width: 0 }}
+    animate={{ width: `${progress}%` }}
+  />
+);
 
 export default ProgressBar;
