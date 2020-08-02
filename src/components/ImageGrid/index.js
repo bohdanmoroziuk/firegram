b9730343a985c65fc449ff9839ed892f2d2b5cbc
Bogdan <bogdan7morozyuk@gmail.com>
2020-08-02 09:29:08 +0300

    refactor(image-grid): add functions to render images

--- src/components/ImageGrid/index.js
+++ src/components/ImageGrid/index.js
@@ -6,33 +6,31 @@ import 'components/ImageGrid/index.css';
 const ImageGrid = ({ selectImage }) => {
   const { documents: images } = useFirestore('images');
 
+  const renderImage = ({ id, url }) => (
+    <motion.div 
+      className="image-wrapper" 
+      key={id}
+      onClick={() => selectImage(url)}
+      whileHover={{ opacity: 1 }}
+      layout
+    >
+      <motion.img 
+        src={url} 
+        alt="" 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+      />
+    </motion.div>
+  )
+
+  const renderImages = images => (
+    images && images.map(renderImage)
+  );
+
   return (
     <div className="image-grid">
-      {images && images.map(({ id, url }) => (
-        <motion.div 
-          className="image-wrapper" 
-          key={id}
-          onClick={() => selectImage(url)}
-          whileHover={{
-            opacity: 1
-          }}
-          layout
-        >
-          <motion.img 
-            src={url} 
-            alt="" 
-            initial={{
-              opacity: 0,
-            }}
-            animate={{
-              opacity: 1
-            }}
-            transition={{
-              delay: 1
-            }}
-          />
-        </motion.div>
-      ))}
+      {renderImages(images)}
     </div>
   );
 };
